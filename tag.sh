0e7fc389dca48e106a01c221508c4f38fde758ff
Weston Steimel <weston.steimel@gmail.com>
2019-08-12 22:11:23 +0100

    remove master tag
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- tag.sh
+++ tag.sh
@@ -5,4 +5,3 @@ VERSION=${VERSION#ARG YOUTUBE_DL_VERSION=\"}
 VERSION=${VERSION%\"}
 echo "Tagging version ${VERSION}"
 docker tag "${DOCKER_USERNAME}/youtube-dl:latest" "${DOCKER_USERNAME}/youtube-dl:${VERSION}"
-docker tag "${DOCKER_USERNAME}/youtube-dl:latest" "${DOCKER_USERNAME}/youtube-dl:master"
